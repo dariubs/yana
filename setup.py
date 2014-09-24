a1bb51c05ddf3104d1e033f77434fb1caf564095
Dariush Abbasi <dariushem@yahoo.com>
2014-09-24 14:35:23 +0330

    add setup files

---
+++ setup.py
@@ -0,0 +1,7 @@
+
+from setuptools import setup
+
+setup(name="yana",
+      description="an extensible static site generator",
+      author="Dariush Abbasi",
+      author_email="poshtehani@gmail.com")
