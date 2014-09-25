154057ab9efec633850544900108d457dd19ff2d
Dariush Abbasi <dariushem@yahoo.com>
2014-09-25 23:58:28 +0330

    change setup

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 
-from setuptools import setup
+from distutils.core import setup
 
-setup(name="yana",
-      description="an extensible static site generator",
-      author="Dariush Abbasi",
-      author_email="poshtehani@gmail.com")
+setup(
+        name        =   "yana",
+        description =   "an extensible static site generator",
+        version     =   "0.0.0",
+        author      =   "Dariush Abbasi",
+        author_email=   "poshtehani@gmail.com"
+    )
