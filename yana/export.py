b0b7e81638618c9642621abc43f9e1e79de4a8c6
Dariush Abbasi <dariushem@yahoo.com>
2014-09-29 10:39:13 +0330

    add export class

---
+++ yana/export.py
@@ -0,0 +1,17 @@
+class Export:
+    def __init__(self):
+        super.__init__()
+
+    def setTheme(self,theme_str):
+        self.theme_str = theme_str;
+        pass
+
+    def setText(self,text_str):
+        self.text_str = text_str;
+        pass
+
+    def parseText(self):
+        pass
+
+    def compile(self):
+        pass
